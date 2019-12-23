ab1fa1db774e9a070751b6109fbb83366904c473
James Bonner <jameseqbonner@gmail.com>
2019-12-23 11:26:58 -0700

    separated html js and css

---
+++ static/functions.js
@@ -0,0 +1,71 @@
+function activate_up() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/activate_up';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function deactivate_up() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/deactivate_up';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function activate_down() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/activate_down';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function deactivate_down() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/deactivate_down';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function activate_left() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/activate_left';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function deactivate_left() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/deactivate_left';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function activate_right() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/activate_right';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
+
+function deactivate_right() {
+	let xhttp = new XMLHttpRequest();
+
+	let url = '/deactivate_right';
+
+	xhttp.open('POST', url);
+	xhttp.send();
+}
